332277740ffcdbd815f2784d7b5bfa2799179b26
Aditya1424 <92393365+Aditya1424@users.noreply.github.com>
2021-10-12 23:00:59 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("enter value of a and b\n");
+	scanf("%d" "%d",&a,&b);
+	a=a*b;
+	b=a/b;
+	a=a/b;
+	printf("swap the number is: %d\n",a);
+	printf("swap the number is: %d\n",b);
+	
+	
+}
