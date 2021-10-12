c1742eafcecc7638c1130bab334766d70cab62e4
Aditya1424 <92393365+Aditya1424@users.noreply.github.com>
2021-10-12 23:09:46 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter value of a\n");
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("even number is: %d\n",a);
+	else{
+		printf("odd number is: %d\n",a);
+		return 0;
+	}
+	}
+	
