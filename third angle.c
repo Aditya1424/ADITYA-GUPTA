4e147211957b05f9547c69409375694526e1c4db
Aditya1424 <92393365+Aditya1424@users.noreply.github.com>
2021-10-14 02:15:20 +0530

    Add files via upload

---
+++ third angle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	float a,b,c;
+	printf("enter the value of a\tb\n ");
+	scanf("%f" "%f",&a,&b);
+	c = (180-(a+b));
+	printf("the third angle is: %f",c);
+	return 0;
+	
+	
+}
